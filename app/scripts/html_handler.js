e5d20b2f46c54718dd8a6d50bace8db693167ad1
Andreas Styltsvig <ansty93@gmail.com>
2019-02-27 12:42:58 +0100

    close #46 impove timetableviewer

--- app/scripts/html_handler.js
+++ app/scripts/html_handler.js
@@ -12,20 +12,34 @@ function addRow(data) {
     cell2 = document.createElement("td");
     cell3 = document.createElement("td");
     cell4 = document.createElement("td");
-    textnode1 = document.createTextNode(data["category"]);
-    textnode2 = document.createTextNode(data["date"]);
+    cell5 = document.createElement("td");
+    cell6 = document.createElement("td");
+    cell7 = document.createElement("td");
+    textnode1 = document.createTextNode(data["date"]);
+    textnode2 = document.createTextNode(data["category"]);
     textnode3 = document.createTextNode(data["project"]);
-    textnode4 = document.createTextNode(data["time_end"]);
+    textnode4 = document.createTextNode(data["time_start"]);
+    textnode5 = document.createTextNode(data["time_end"]);
+    textnode6 = document.createTextNode("0");
+    textnode7 = document.createTextNode(data["comment"]);
     cell1.appendChild(textnode1);
     cell2.appendChild(textnode2);
     cell3.appendChild(textnode3);
     cell4.appendChild(textnode4);
+    cell5.appendChild(textnode5);
+    cell6.appendChild(textnode6);
+    cell7.appendChild(textnode7);
     row.appendChild(cell1);
     row.appendChild(cell2);
     row.appendChild(cell3);
     row.appendChild(cell4);
+    row.appendChild(cell5);
+    row.appendChild(cell6);
+    row.appendChild(cell7);
+    console.log(textnode7)
     tabBody.appendChild(row);
 }
+
 
 
 function insertAllCategories(categoryArray) {
