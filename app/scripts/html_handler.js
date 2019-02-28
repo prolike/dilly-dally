a2267778439cf1517a764f9d6cc3cc860c0bac72
cph-lh <tass2012@gmail.com>
2019-02-28 16:47:43 +0100

    close #52 featureinvoice report

--- app/scripts/html_handler.js
+++ app/scripts/html_handler.js
@@ -1,3 +1,54 @@
+function insertAllRowsProject(data) {
+    removeAllRows();
+     for (var obj in data) {
+         console.log(data[obj])
+         addRowProject(data[obj])
+     }
+ }
+ 
+ 
+ function addRowProject(data) {
+    console.log(data)
+     tabBody = document.getElementsByTagName("tbody").item(0);
+     row = document.createElement("tr");
+     cell1 = document.createElement("td");
+     cell2 = document.createElement("td");
+     cell3 = document.createElement("td");
+     cell4 = document.createElement("td");
+     cell5 = document.createElement("td");
+     cell6 = document.createElement("td");
+     cell7 = document.createElement("td");
+     cell8 = document.createElement("td");
+     textnode1 = document.createTextNode(data["date"]);
+     textnode2 = document.createTextNode(data["category"]);
+     textnode3 = document.createTextNode(data["project"]);
+     textnode4 = document.createTextNode(data["time_start"]);
+     textnode5 = document.createTextNode(data["time_end"]);
+     textnode6 = document.createTextNode(data["hours"]);
+     textnode7 = document.createTextNode(data["comment"]);
+
+     cell1.appendChild(textnode1);
+     cell2.appendChild(textnode2);
+     cell3.appendChild(textnode3);
+     cell4.appendChild(textnode4);
+     cell5.appendChild(textnode5);
+     cell6.appendChild(textnode6);
+     cell7.appendChild(textnode7);
+     row.appendChild(cell1);
+     row.appendChild(cell2);
+     row.appendChild(cell3);
+     row.appendChild(cell4);
+     row.appendChild(cell5);
+     row.appendChild(cell6);
+     row.appendChild(cell7);
+     tabBody.appendChild(row);
+ }
+ 
+ 
+
+
+
+
 function insertAllRows(data) {
    removeAllRows();
     for (var obj in data) {
