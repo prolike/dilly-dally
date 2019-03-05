59c9e0ede7f57505e98c00667617cb3e2b147818
Andreas Styltsvig <ansty93@gmail.com>
2019-03-05 10:22:31 +0100

    close #71 set a initial test framework for functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
+
+
+// Functions called from webapp
+ exports.webappapi = functions.https.onCall((data, context) => {
+  console.log(data)
+  console.log(context)
+  return "asd"
+ });
+
+ exports.api = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
  });
+ 
+
+
+exports.getAllProjects = function(customerName, projectName, data){
+ 	console.log(customerName,projectName,data)
+ } 
+exports.getAllCategories = function(customerName, projectName, data){
+ 	console.log(customerName,projectName,data)
+ } 
+
+ exports.getAllTimeRegistrations = function(customerName, projectName, data){
+ 	console.log(customerName,projectName,data)
+ } 
+
+ exports.timeRegistrationAdd = function(customerName, projectName, data){
+ 	console.log(customerName,projectName,data)
+ } 
+
+ exports.timeRegistrationRemove = function(customerName, projectName, timeRegistrationID){
+ 	console.log(customerName,projectName,timeRegistrationID)}
+ 
+
---
+++ functions/test/index.test.js
@@ -0,0 +1,26 @@
+const myFunctions = require('../index.js');
+const fft = require("firebase-functions-test")();
+var expect = require('chai').expect
+var sinon = require('sinon');
+const httpMocks       = require('node-mocks-http');
+const eventEmitter    = require('events').EventEmitter;
+const nock = require('nock')
+
+
+// https://stackoverflow.com/questions/49352060/how-do-you-unit-test-a-firebase-function-wrapped-with-express
+describe('#### API TEST - Testing our API entrypoint ####', function(done) {
+
+  it('Initial test', function (done) {
+    const data = {foo: 'bar'};
+    const callableContext = {auth: {uid: 'abc'}}; // you can also add "rawRequest" or "instanceIdToken" fields
+
+    const wrapped = fft.wrap(myFunctions.webappapi);
+    const result = wrapped(data,callableContext);
+    expect(result).to.equal("asd")
+    //console.log(res)
+    //console.log(res)
+    done();
+  });
+
+
+});
