6cbf0fee311addb3f168c77e56c329f0514b0e0a
Ansty93 <ansty93@gmail.com>
2019-03-12 12:07:08 +0100

    closing #96

--- app/src/views/login/Login.vue
+++ app/src/views/login/Login.vue
@@ -1,25 +1,62 @@
 <template>
-  <div id="firebaseui-auth-container"></div>
+
+  <body>
+    <center>
+      <!-- The surrounding HTML is left untouched by FirebaseUI.
+         Your app may use that space for branding, controls and other customizations.-->
+      <h1>Welcome to My Awesome App</h1>
+      <div id="firebaseui-auth-container"></div>
+      <div v-if=authenticated>
+        <vue-circle :progress="50" :size="100" :reverse="false" line-cap="round" :fill="fill" empty-fill="rgba(0, 0, 0, .1)" :animation-start-value="0.0" :start-angle="0" insert-mode="append" :thickness="5" :show-percent="true" @vue-circle-progress="progress" @vue-circle-end="progress_end">
+          <p>Slot!</p>
+        </vue-circle>
+      </div>
+    </center>
+  </body>
 </template>
 <script>
+import VueCircle from 'vue2-circle-progress'
 import firebase from 'firebase';
 import firebaseui from 'firebaseui';
 
 export default {
   name: 'Login',
+  components: {
+    VueCircle
+  },
+  data() {
+    return {
+      fill: { gradient: ["red", "green", "blue"] },
+      authenticated: false
+    }
+  },
+  methods: {
+    progress(event, progress, stepValue) {
+      console.log(stepValue);
+    },
+    progress_end(event) {
+      console.log("Circle progress end");
+    }
+  },
   mounted() {
     var uiConfig = {
       signInSuccessUrl: '/#/home/dashboard',
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      ]
-    };
-    let ui = firebaseui.auth.AuthUI.getInstance();
-    if (!ui) {
-      ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ],
+      callbacks: {
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          console.log("asd")
+        }
+      }};
+      let ui = firebaseui.auth.AuthUI.getInstance();
+      if (!ui) {
+        ui = new firebaseui.auth.AuthUI(firebase.auth());
+      }
+      ui.start('#firebaseui-auth-container', uiConfig);
     }
-    ui.start('#firebaseui-auth-container', uiConfig);
   }
-}
 
 </script>
