b715cc619b07c3b24f7abc19201cea521b01bb87
Ansty93 <ansty93@gmail.com>
2019-03-13 14:22:26 +0100

    close #114

--- app/src/views/login/Login.vue
+++ app/src/views/login/Login.vue
@@ -1,31 +1,26 @@
 <template>
-
   <section class="loginPage">
-    <img
-          class="loginLogo"
-          src="img/brand/prolike-logo.svg"
-          width="89"
-          height="25"
-          alt="Prolike Logo"
-        >
+    <img class="loginLogo" src="img/brand/prolike-logo.svg" width="89" height="25" alt="Prolike Logo">
     <h1>Time Registration</h1>
-   <button id="googleLogin" v-on:click="test"><img class="googleIcon" src="img/google-icon.png">Sign in with Google</button>
+    <button id="googleLogin" v-on:click="test"><img class="googleIcon" src="img/google-icon.png">Sign in with Google</button>
   </section>
 </template>
 <script>
 import VueCircle from 'vue2-circle-progress'
 import firebase from 'firebase';
-import firebaseui from 'firebaseui';
+import router from '../../router/index.js'
 
 export default {
   name: 'Login',
+  router,
   components: {
-    VueCircle
+    VueCircle,
   },
   data() {
     return {
       fill: { gradient: ["red", "green", "blue"] },
-      authenticated: false
+      authenticated: false,
+      provider: ""
     }
   },
   methods: {
@@ -35,32 +30,39 @@ export default {
     progress_end(event) {
       console.log("Circle progress end");
     },
-    test(){
-      console.log("hha")
+    test() {
+      console.log("clicked")
+      firebase.auth().signInWithRedirect(this.provider);
+
     },
-    signIn(){
-          var uiConfig = {
-      signInSuccessUrl: '/#/home/dashboard',
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      ],
-      callbacks: {
-        uiShown: function() {
-          // The widget is rendered.
-          // Hide the loader.
-          console.log("asd")
-        }
-      }};
-      let ui = firebaseui.auth.AuthUI.getInstance();
-      if (!ui) {
-        ui = new firebaseui.auth.AuthUI(firebase.auth());
-      }
-      
-    }
   },
   mounted() {
+    this.provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        console.log("success")
+        window.location.href = "/#/home/dashboard";
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log(error)
 
-    }
+      console.log("failed")
+      // ...
+    });
+
   }
+}
 
 </script>
