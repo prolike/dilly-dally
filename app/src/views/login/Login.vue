48d1698e0a7774eef8c8759734914bdfe931ffc4
Ansty93 <ansty93@gmail.com>
2019-03-14 09:58:54 +0100

    close #116

--- app/src/views/login/Login.vue
+++ app/src/views/login/Login.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="loginPage">
-
     <!--Her Andreas, her er hvad du skal lave i JS!-->
-
-    <!--<img class="loginLogo" src="img/brand/prolike-logo.svg" width="89" height="25" alt="Prolike Logo">
-    <h1>Time Registration</h1>
-    <button id="googleLogin" v-on:click="test"><img class="googleIcon" src="img/google-icon.png">Sign in with Google</button>-->
-    <div><img class="spinner-logo" src="img/yellow-logo.png" alt=""></div>
-    <h2>Authenticating . . .</h2>
+    <div v-if="seen2"><img class="loginLogo" src="img/brand/prolike-logo.svg" width="89" height="25" alt="Prolike Logo">
+      <h1>Time Registration</h1>
+      <button id="googleLogin" v-on:click="test"><img class="googleIcon" src="img/google-icon.png">Sign in with Google</button>
+    </div>
+    <div v-if="seen">
+      <center>
+        <img class="spinner-logo" src="img/yellow-logo.png" alt="">
+        <h2>Authenticating . . .</h2>
+      </center>
+    </div>
   </section>
 </template>
 <script>
@@ -25,7 +28,9 @@ export default {
     return {
       fill: { gradient: ["red", "green", "blue"] },
       authenticated: false,
-      provider: ""
+      provider: "",
+      seen: false,
+      seen2: true
     }
   },
   methods: {
@@ -35,37 +40,44 @@ export default {
     progress_end(event) {
       console.log("Circle progress end");
     },
-    test() {
-      console.log("clicked")
-      firebase.auth().signInWithRedirect(this.provider);
+    initFirebaseLogin() {
 
     },
-  },
-  mounted() {
-    this.provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
+    test() {
+      this.setLoginState()
+      console.log("clicked")
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
-        console.log("success")
+        // The signed-in user info.
+        var user = result.user;
         window.location.href = "/#/home/dashboard";
-        // ...
-      }
-      // The signed-in user info.
-      var user = result.user;
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      console.log(error)
+        this.setdefaultState()
 
-      console.log("failed")
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
       // ...
-    });
+    },
+    setdefaultState() {
+      this.seen = false
+      this.seen2 = true
+    },
+    setLoginState() {
+      this.seen = true
+      this.seen2 = false
+    }
+  },
+  mounted() {
 
   }
 }
