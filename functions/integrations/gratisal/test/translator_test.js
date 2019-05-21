c6e675c551f8ea1f261aba9fb69e1d291c2a9bbe
Andreas S. Styltsvig <ansty93@gmail.com>
2019-05-21 12:25:09 +0200

    Adding more test #262

--- functions/integrations/gratisal/test/translator_test.js
+++ functions/integrations/gratisal/test/translator_test.js
@@ -1,12 +1,17 @@
 var translator = require('../translator.js');
 var sinon = require('sinon');
+var chai = require('chai')
 var expect = require('chai').use(require('sinon-chai')).expect;
 const httpMocks = require('node-mocks-http');
 const eventEmitter = require('events').EventEmitter;
 const nock = require('nock')
-var request = require('request');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
 
 
+let employmentsOverview = require('./data/employmentsOverview.json');
+let timeregistration1 = require('./data/timeregistration1.json');
 
 
 describe('#### UNIT TEST ####', function() {
@@ -15,144 +20,82 @@ describe('#### UNIT TEST ####', function() {
 
     });
 
+    it('should format the date correctly', function(done) {
+        var timestamp = {
+            seconds: 1558342800,
+            nanoseconds: 0
+        }
+        var expectedResult = "2019-05-20T09:00:00"
+        var result = translator.formatDate(timestamp);
+        expect(expectedResult).to.equal(result)
+        done();
+    });
 
+    it('should find the UserEmploymentId from employmentsOverview by fullName', function(done) {
+        var fullName = "Laura Hartig"
+        var expectedResult = 3293
+        var result = translator.getUserEmployementIDbyFullName(employmentsOverview, fullName);
+        expect(expectedResult).to.equal(result)
+        done();
+    });
 });
 
 
 describe('#### UNIT TEST WITH MOCK ####', function() {
 
     var url = "https://api.gratisaltest.dk"
+
+
     beforeEach(function() {
 
     });
 
-    it('should send a post request and query for the token', function(done) {
+    it('should send a POST request and query for the token', function(done) {
         //Mock server 
+        var token = "something"
         const scope = nock(url) //Api url
             .post('/api/auth/login') //The url-path we are going to recieve HTTP request on
             .reply(function(uri, requestBody) { // The reply function
-                //console.log('path:', this.req)
-                console.log('headers:', this.req.headers)
-                //console.log('headers:', requestBody)
                 var auth = this.req.headers.authorization.split(" ")
-                let buff = new Buffer(auth[1], 'base64');  
+                var buff = new Buffer.from(auth[1], 'base64');
                 var decodedAuth = buff.toString("ascii")
-                console.log(decodedAuth)
-                var reply = {Token : "something"}
+                var reply = {
+                    Token: token
+                }
                 expect(auth[0]).to.equal("Basic")
                 expect(decodedAuth).to.include(":")
-                return reply
+                return [200, reply]
             })
-        //.log(console.log)
-        translator.getToken();
-        done();
+            //.log(console.log)
+        translator.getToken().should.eventually.equal(token).notify(done)
     });
 
-    it('should send a post request and query for the token', function(done) {
-        //Mock server 
-        const scope = nock(url) //Api url
-            .post('/api/auth/login') //The url-path we are going to recieve HTTP request on
-            .reply(function(uri, requestBody) { // The reply function
-                //console.log('path:', this.req)
-                console.log('headers:', this.req.headers)
-                //console.log('headers:', requestBody)
-                var authBasic = this.req.headers.authorization.split(" ")
-                var reply = {Token : "something"}
-                expect(authBasic[0]).to.equal("Basic")
-                return reply
+    it('should send a GET request and query for the employments overview', function(done) {
+        var token = "something"
+        const scope = nock(url)
+            .get('/api/employments/overview')
+            .reply(function(uri, requestBody) {
+                var reqToken = this.req.headers.authorization
+                var reply = employmentsOverview
+                var expectedToken = "Token " + token
+                expect(expectedToken).to.equal(reqToken)
+                return [200, reply]
             })
-        //.log(console.log)
-        translator.getToken();
-        done();
+        translator.getEmployments(token).should.eventually.equal(employmentsOverview).notify(done)
     });
+
+    /*it('should send a POST request and query for timeEntry', function(done) {
+        var token = "something"
+        const scope = nock(url)
+            .post('/api/timeentry')
+            .reply(function(uri, requestBody) {
+                var reqToken = this.req.headers.authorization
+                var reply = employmentsOverview
+                var expectedToken = "Token " + token
+                expect(token).to.equal(reqToken)
+                return [200, reply]
+            })
+        translator.postTimeEntry(token,data).should.eventually.equal(employmentsOverview).notify(done)
+    });*/
 
 });
-/*
-var data = {
-        category: {
-            cost: "0",
-            id: "Master",
-            price: "2100"
-        }
-        comment: "M.A.R.S. strategi"
-        cost: "0",
-        endTime: "March 15, 2019 at 12:00:00 PM UTC+1",
-        issues: null,
-        paidMonth: "",
-        price: "6300",
-        project: {
-            categories: {
-                Explore: {
-                    price: 0
-                },
-                Master: {
-                    price: 2100
-                },
-                Prolike: {
-                    price: 330
-                },
-                customer: {
-                    name: "CCC",
-                    id "CCC"
-                },
-            }
-        }
-        startTime: "March 15, 2019 at 9:00:00 AM UTC+1",
-        status: "OK",
-        worker: { id: "lakruzz@prolike.io" }
-    }
-
-
-{
-    category: {
-        cost: "0",
-        id: "Master",
-        price: "2100"
-    }
-    comment: "M.A.R.S. strategi"
-    cost: "0",
-    endTime: "March 15, 2019 at 12:00:00 PM UTC+1",
-    issues: null,
-    paidMonth: "",
-    price: "6300",
-    project: {
-        categories: {
-            Explore: {
-                price: 0
-            },
-            Master: {
-                price: 2100
-            },
-            Prolike: {
-                price: 330
-            },
-            customer: {
-                name: "CCC",
-                id "CCC"
-            },
-        }
-    }
-    startTime: "March 15, 2019 at 9:00:00 AM UTC+1",
-    status: "OK",
-    worker: { id: "lakruzz@prolike.io" }
-    */
-
-
-/*
-
-    {
-  "UnitType": {
-    "Name": "Prolike",
-  },
- "Status": {
-            "Name": "Åben",
-        },
- "EmployeeName": "Andreas",
-        "UserEmploymentId": 3289,
-        "EntryDate": "2019-04-14T00:00:00",
-        "TimeEntryTypeId": 12217,
-        "UnitTypeId": 2,
-        "Description": "Working on gratisal - #125125125",
-        "SalaryPeriodId": 111,
-
-        */
--- functions/integrations/gratisal/translator.js
+++ functions/integrations/gratisal/translator.js
@@ -29,19 +29,29 @@ exports.gratisalTimeentry = functions.https.onRequest((request, response) => {
 });
 
 exports.parseData = function(data) {
-    
+    return ""
 }
 
-exports.getAllEmployees = function() {
+
+exports.formatDate = function(timestamp){
+     var seconds = timestamp.seconds
+     var date = new Date(seconds*1000)
+     var formattedDate = date.toJSON().split(".")[0]
+     return formattedDate
 }
 
-exports.getAllCategories = function() {
 
+exports.getUserEmployementIDbyFullName = function(employmentsOverview, fullName) {
+    var obj = employmentsOverview.find(item => item.FullName === fullName)
+    var UserEmploymentId = obj.UserEmploymentId
+    return UserEmploymentId
 }
+
+
 
 exports.getToken = function() {
     return new Promise(function(resolve, reject) {
-        auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+        auth = "Basic " + new Buffer.from(username + ":" + password).toString("base64");
         var options = {
             method: 'POST',
             url: URL + "/api/auth/login",
@@ -49,13 +59,13 @@ exports.getToken = function() {
                 'Authorization': auth
             }
         };
+
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body);
-                var token = info.Token
+                var data = JSON.parse(body);
+                var token = data.Token
                 resolve(token)
-            }
-            else{
+            } else {
                 reject(error)
             }
         }
@@ -63,6 +73,52 @@ exports.getToken = function() {
     })
 }
 
-exports.sendToGratisal = function(data) {
+exports.getEmployments = function(token) {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            method: 'GET',
+            url: URL + "/api/employments/overview",
+            headers: {
+                'Authorization': "Token " + token
+            }
+        };
 
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body);
+                resolve(data)
+            } else {
+                reject(error)
+            }
+        }
+        req(options, callback);
+    })
+}
+
+exports.getAllCategories = function() {
+
+}
+
+
+exports.postTimeEntry = function(token, data) {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            method: 'POST',
+            url: URL + "/api/auth/login",
+            headers: {
+                'Authorization': "Token " + token
+            }
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                var token = info.Token
+                resolve(token)
+            } else {
+                reject(error)
+            }
+        }
+        req(options, callback);
+    })
 }
